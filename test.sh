83e9303f8627379764bfc3d406b6849d0fcce561
Yugo Mizuniwa <ymizuniw@Yugos-MacBook-Air.local>
2025-12-17 23:33:20 +0900

    test script updated

--- test.sh
+++ test.sh
@@ -11,18 +11,18 @@ else
     DFLAG="DEBUG=0"
 fi
 
-printf "==============================ex00===========================\n\n"
+printf "==============================ex00=============================\n\n"
 cd ex00 && make "$DFLAG" 1>/dev/null && ./test_without_messages.sh
 printf "\n"
 cd ..
-printf "==============================ex01===========================\n\n"
+printf "==============================ex01=============================\n\n"
 cd ex01 && make "$DFLAG" 1>/dev/null && ./test_without_messages.sh
 printf "\n"
 cd ..
-printf "==============================ex02===========================\n\n"
+printf "==============================ex02=============================\n\n"
 cd ex02 && make "$DFLAG" 1>/dev/null  && ./test_without_messages.sh
 printf "\n"
 cd ..
-printf "==============================ex03===========================\n\n"
+printf "==============================ex03=============================\n\n"
 cd ex03 && make "$DFLAG" 1>/dev/null && ./test_without_messages.sh
 printf "\n"
