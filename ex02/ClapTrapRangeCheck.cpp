38e853ff95a4d99c16ac234c3ddf89776b51f201
Yugo Mizuniwa <ymizuniw@Yugos-MacBook-Air.local>
2025-12-17 21:58:01 +0900

    pedantic-errors fixed

--- ex02/ClapTrapRangeCheck.cpp
+++ ex02/ClapTrapRangeCheck.cpp
@@ -1,20 +1,21 @@
 #include <ClapTrap.hpp>
 #include <limits>
+#include <stdint.h>
 
 bool uint_sum_overflow(unsigned int n1, unsigned int n2) {
-  unsigned long long ui = 0;
+  uint64_t ui;
+  ui = 0;
   ui = n1 + n2;
-  if (ui >
-      static_cast<unsigned long long>(std::numeric_limits<unsigned int>::max()))
+  if (ui > static_cast<uint64_t>(std::numeric_limits<unsigned int>::max()))
     return true;
   return (false);
 }
 
 bool uint_mult_overflow(unsigned int n1, unsigned int n2) {
-  unsigned long long ui = 0;
+  uint64_t ui;
+  ui = 0;
   ui = n1 * n2;
-  if (ui >
-      static_cast<unsigned long long>(std::numeric_limits<unsigned int>::max()))
+  if (ui > static_cast<uint64_t>(std::numeric_limits<unsigned int>::max()))
     return true;
   return (false);
 }
