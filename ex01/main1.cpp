7a41134525493af340da529cb107d80462f3fcf9
Yugo Mizuniwa <ymizuniw@c6r2s14.42tokyo.jp>
2025-12-19 17:36:57 +0900

    tiny fix

--- ex01/main1.cpp
+++ ex01/main1.cpp
@@ -1,12 +1,12 @@
 #include <ScavTrap.hpp>
 
 int main(void) {
-  ScavTrap St0; // default constructor call
+  ScavTrap St0; // default constructor
   ScavTrap St1("Scav1");
   ScavTrap St2("Scav2");
   ScavTrap St3("Scav3");
-  ScavTrap St4(St3);     // equal to St3
-  ScavTrap St5("Scav5"); // equal to St5
+  ScavTrap St4(St3); // equal to St3
+  ScavTrap St5("Scav5");
   St5 = St4;
 #ifdef DEBUG
   St1.print_status();
@@ -16,6 +16,8 @@ int main(void) {
   St5.print_status();
 #endif
   St1.attack("Scav2");
+  St4.attack("Scav5"); // St4=St3 attacks Scav5.
+  St5.takeDamage(DEFAULT_SCAVTRAP_ATTACK);
   St2.takeDamage(DEFAULT_SCAVTRAP_ATTACK);
   St3.beRepaired(DEFAULT_SCAVTRAP_REPAIRE);
   St4.guradGate();
--- ex03/main1.cpp
+++ ex03/main1.cpp
@@ -5,8 +5,8 @@ int main(void) {
   ScavTrap St1("Scav1");
   ScavTrap St2("Scav2");
   ScavTrap St3("Scav3");
-  ScavTrap St4(St3);     // equal to St3
-  ScavTrap St5("Scav5"); // equal to St5
+  ScavTrap St4(St3); // equal to St3
+  ScavTrap St5("Scav5");
   St5 = St4;
 #ifdef DEBUG
   St1.print_status();
